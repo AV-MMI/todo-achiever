ee2d2673147f38b4c726370e10723036884ede6a
AV-MMI <villalobos.aaron@hotmail.com>
2023-09-05 17:57:37 -0400

    add basic project structure. add basic markup structure

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+
+  entry: {
+  	index: './src/index.js',
+  },
+
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+    	template: './src/index.html',
+    })
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/src'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+    ],
+  },
+
+  optimization: {
+  	runtimeChunk: 'single',
+  },
+};
